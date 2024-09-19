5dcaa71201413b3d44c65879975a3f34bd0f5762
Cameron311 <cwitz17@gamil.com>
2024-09-19 15:06:53 -0400

    Add JSON for multiple redirects, refactor redirect tests

--- src/test/java/edu/bsu/cs/JsonTranslatorTest.java
+++ src/test/java/edu/bsu/cs/JsonTranslatorTest.java
@@ -7,12 +7,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class JsonTranslatorTest {
-    JsonTranslator jsonTranslator = new JsonTranslator("{\"continue\":{\"rvcontinue\":\"20240821130526|1241489226\",\"continue\":\"||\"},\"query\":{\"redirects\":[{\"from\":\"Zappa\",\"to\":\"Frank Zappa\"}],\"pages\":[{\"pageid\":10672,\"ns\":0,\"title\":\"Frank Zappa\",\"revisions\":[{\"user\":\"Willem247\",\"timestamp\":\"2024-09-11T23:03:49Z\"},{\"user\":\"Willem247\",\"timestamp\":\"2024-09-11T23:03:06Z\"},{\"user\":\"GreenC bot\",\"timestamp\":\"2024-09-11T06:24:43Z\"},{\"user\":\"Theworldismovingon2022\",\"timestamp\":\"2024-09-10T16:13:29Z\"},{\"user\":\"Kjell Knudde\",\"timestamp\":\"2024-09-07T21:05:19Z\"},{\"user\":\"Aaw1989\",\"timestamp\":\"2024-09-07T19:09:58Z\"},{\"user\":\"Eurukleia\",\"timestamp\":\"2024-09-05T04:31:51Z\"},{\"user\":\"Eurukleia\",\"timestamp\":\"2024-09-05T04:30:19Z\"},{\"user\":\"Aaw1989\",\"timestamp\":\"2024-09-03T21:27:38Z\"},{\"user\":\"Mad420\",\"timestamp\":\"2024-08-29T00:32:05Z\"},{\"user\":\"Aaw1989\",\"timestamp\":\"2024-08-27T16:34:04Z\"},{\"user\":\"Citation bot\",\"timestamp\":\"2024-08-25T15:17:53Z\"},{\"user\":\"Aaw1989\",\"timestamp\":\"2024-08-24T08:15:54Z\"},{\"user\":\"Aaw1989\",\"timestamp\":\"2024-08-24T08:08:03Z\"},{\"user\":\"RobertG\",\"timestamp\":\"2024-08-22T11:19:56Z\"}]}]}}");
-
+    JsonTranslator defaultJsonTranslator = new JsonTranslator("{\"continue\":{\"rvcontinue\":\"20240821130526|1241489226\",\"continue\":\"||\"},\"query\":{\"redirects\":[{\"from\":\"Zappa\",\"to\":\"Frank Zappa\"}],\"pages\":[{\"pageid\":10672,\"ns\":0,\"title\":\"Frank Zappa\",\"revisions\":[{\"user\":\"Willem247\",\"timestamp\":\"2024-09-11T23:03:49Z\"},{\"user\":\"Willem247\",\"timestamp\":\"2024-09-11T23:03:06Z\"},{\"user\":\"GreenC bot\",\"timestamp\":\"2024-09-11T06:24:43Z\"},{\"user\":\"Theworldismovingon2022\",\"timestamp\":\"2024-09-10T16:13:29Z\"},{\"user\":\"Kjell Knudde\",\"timestamp\":\"2024-09-07T21:05:19Z\"},{\"user\":\"Aaw1989\",\"timestamp\":\"2024-09-07T19:09:58Z\"},{\"user\":\"Eurukleia\",\"timestamp\":\"2024-09-05T04:31:51Z\"},{\"user\":\"Eurukleia\",\"timestamp\":\"2024-09-05T04:30:19Z\"},{\"user\":\"Aaw1989\",\"timestamp\":\"2024-09-03T21:27:38Z\"},{\"user\":\"Mad420\",\"timestamp\":\"2024-08-29T00:32:05Z\"},{\"user\":\"Aaw1989\",\"timestamp\":\"2024-08-27T16:34:04Z\"},{\"user\":\"Citation bot\",\"timestamp\":\"2024-08-25T15:17:53Z\"},{\"user\":\"Aaw1989\",\"timestamp\":\"2024-08-24T08:15:54Z\"},{\"user\":\"Aaw1989\",\"timestamp\":\"2024-08-24T08:08:03Z\"},{\"user\":\"RobertG\",\"timestamp\":\"2024-08-22T11:19:56Z\"}]}]}}");
+    JsonTranslator multipleRedirectsJsonTranslator = new JsonTranslator("{\"continue\":{\"rvcontinue\":\"20240821130526|1241489226\",\"continue\":\"||\"},\"query\":{\"redirects\":[{\"from\":\"Zappa\",\"to\":\"Frank Zappa\"},{\"from\":\"Frank Zappa\",\"to\":\"Elephant\"}],\"pages\":[{\"pageid\":10672,\"ns\":0,\"title\":\"Frank Zappa\",\"revisions\":[{\"user\":\"Willem247\",\"timestamp\":\"2024-09-11T23:03:49Z\"},{\"user\":\"Willem247\",\"timestamp\":\"2024-09-11T23:03:06Z\"},{\"user\":\"GreenC bot\",\"timestamp\":\"2024-09-11T06:24:43Z\"},{\"user\":\"Theworldismovingon2022\",\"timestamp\":\"2024-09-10T16:13:29Z\"},{\"user\":\"Kjell Knudde\",\"timestamp\":\"2024-09-07T21:05:19Z\"},{\"user\":\"Aaw1989\",\"timestamp\":\"2024-09-07T19:09:58Z\"},{\"user\":\"Eurukleia\",\"timestamp\":\"2024-09-05T04:31:51Z\"},{\"user\":\"Eurukleia\",\"timestamp\":\"2024-09-05T04:30:19Z\"},{\"user\":\"Aaw1989\",\"timestamp\":\"2024-09-03T21:27:38Z\"},{\"user\":\"Mad420\",\"timestamp\":\"2024-08-29T00:32:05Z\"},{\"user\":\"Aaw1989\",\"timestamp\":\"2024-08-27T16:34:04Z\"},{\"user\":\"Citation bot\",\"timestamp\":\"2024-08-25T15:17:53Z\"},{\"user\":\"Aaw1989\",\"timestamp\":\"2024-08-24T08:15:54Z\"},{\"user\":\"Aaw1989\",\"timestamp\":\"2024-08-24T08:08:03Z\"},{\"user\":\"RobertG\",\"timestamp\":\"2024-08-22T11:19:56Z\"}]}]}}");
     @Test
     public void test_getEntriesMatchingTimestampAsString() {
         String expectedListOfTimestampsAsString = "[\"2024-09-11T23:03:49Z\",\"2024-09-11T23:03:06Z\",\"2024-09-11T06:24:43Z\",\"2024-09-10T16:13:29Z\",\"2024-09-07T21:05:19Z\",\"2024-09-07T19:09:58Z\",\"2024-09-05T04:31:51Z\",\"2024-09-05T04:30:19Z\",\"2024-09-03T21:27:38Z\",\"2024-08-29T00:32:05Z\",\"2024-08-27T16:34:04Z\",\"2024-08-25T15:17:53Z\",\"2024-08-24T08:15:54Z\",\"2024-08-24T08:08:03Z\",\"2024-08-22T11:19:56Z\"]";
-        String actualListOfTimestampsAsString = jsonTranslator.getEntriesMatchingTargetAsString("timestamp");
+        String actualListOfTimestampsAsString = defaultJsonTranslator.getEntriesMatchingTargetAsString("timestamp");
 
         Assertions.assertEquals(expectedListOfTimestampsAsString, actualListOfTimestampsAsString);
     }
@@ -36,7 +36,7 @@ public class JsonTranslatorTest {
                 "2024-08-24T08:08:03Z",
                 "2024-08-22T11:19:56Z"
         );
-        List<String> actualListOfTimestamps = jsonTranslator.getEntriesMatchingTargetAsList("timestamp");
+        List<String> actualListOfTimestamps = defaultJsonTranslator.getEntriesMatchingTargetAsList("timestamp");
 
         Assertions.assertEquals(expectedListOfTimestamps, actualListOfTimestamps);
     }
@@ -44,7 +44,7 @@ public class JsonTranslatorTest {
     @Test
     public void test_getEntriesMatchingUsersAsString() {
         String expectedListOfUsersAsString = "[\"Willem247\",\"Willem247\",\"GreenC bot\",\"Theworldismovingon2022\",\"Kjell Knudde\",\"Aaw1989\",\"Eurukleia\",\"Eurukleia\",\"Aaw1989\",\"Mad420\",\"Aaw1989\",\"Citation bot\",\"Aaw1989\",\"Aaw1989\",\"RobertG\"]";
-        String actualListOfUsersAsString = jsonTranslator.getEntriesMatchingTargetAsString("user");
+        String actualListOfUsersAsString = defaultJsonTranslator.getEntriesMatchingTargetAsString("user");
 
         Assertions.assertEquals(expectedListOfUsersAsString, actualListOfUsersAsString);
     }
@@ -68,22 +68,22 @@ public class JsonTranslatorTest {
                 "Aaw1989",
                 "RobertG"
         );
-        List<String> actualListOfUsers = jsonTranslator.getEntriesMatchingTargetAsList("user");
+        List<String> actualListOfUsers = defaultJsonTranslator.getEntriesMatchingTargetAsList("user");
 
         Assertions.assertEquals(expectedListOfUsers, actualListOfUsers);
     }
 
     @Test
     public void test_getRevisionAsString(){
-        String expected = "[\"Frank Zappa\"]";
-        String actual = jsonTranslator.getEntriesMatchingTargetAsString("redirects[-1].to");
+        String expected = "[\"Elephant\"]";
+        String actual = multipleRedirectsJsonTranslator.getEntriesMatchingTargetAsString("redirects[-1].to");
         Assertions.assertEquals(expected,actual);
     }
 
     @Test
     public void test_getRevisionAsList(){
-        List<String> expected = Arrays.asList("Frank Zappa");
-        List<String> actual = jsonTranslator.getEntriesMatchingTargetAsList("redirects[-1].to");
+        List<String> expected = Arrays.asList("Elephant");
+        List<String> actual = multipleRedirectsJsonTranslator.getEntriesMatchingTargetAsList("redirects[-1].to");
         Assertions.assertEquals(expected,actual);
     }
 }
