5028f3727a8f9785eb1f08cf0b5afa85ff40dfaa
Kat R. <153114658+katrazo@users.noreply.github.com>
2024-09-24 13:39:10 -0400

    Add basic encoding to WikiApiFetcher, refactor, add code to ConsoleIO

--- src/main/java/edu/bsu/cs/ConsoleIO.java
+++ src/main/java/edu/bsu/cs/ConsoleIO.java
@@ -1,11 +1,14 @@
 package edu.bsu.cs;
 
+import java.util.Scanner;
+
 public class ConsoleIO {
-    public static void main(String[] args) throws ClassNotFoundException {
-        //URLConnector does not exist yet
-//        System.out.println(new URLConnector().doTheThing());
-    }
-    private static String doTheThing(){
-        return "Giraffe";
+    @SuppressWarnings("unused")
+    // For some reason, it warns that String[] args is never used. Did I miss something?
+    public static void main(String[] args) throws Exception {
+        Scanner consoleScanner = new Scanner(System.in);
+        System.out.printf("%nEnter the name of a Wikipedia article.%n> ");
+
+        System.out.println(WikiApiFetcher.fetchArticleInformationWithTitle(consoleScanner.nextLine()));
     }
 }
--- src/main/java/edu/bsu/cs/WikiApiFetcher.java
+++ src/main/java/edu/bsu/cs/WikiApiFetcher.java
@@ -2,19 +2,39 @@ package edu.bsu.cs;
 
 import java.io.InputStream;
 import java.net.*;
+import java.util.Scanner;
 
 public class WikiApiFetcher {
     public static WebPageData fetchArticleInformationWithTitle(String title) throws Exception {
-        URI uriForArticle = new URI((String.format("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=%s&redirects=1&formatversion=2&rvprop=timestamp%s7Cuser&rvlimit=15", title, "%")));
+        StringBuilder titleFixer = new StringBuilder();
+        for (int i = 0; i < title.length(); i++) {
+            String letter = title.substring(i, i+1);
+            if (letter.equals(" "))
+                letter = "+";
+
+            titleFixer.append(letter);
+        }
+
+        URI uriForArticle = new URI(
+                (String.format(
+                        "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=%s&redirects=1&formatversion=2&rvprop=timestamp%s7Cuser&rvlimit=15",
+                        titleFixer, "%")
+                )
+        );
         URL urlForArticle = uriForArticle.toURL();
 
         URLConnection connectionForArticle = urlForArticle.openConnection();
         connectionForArticle.setRequestProperty("User-Agent", "Revision Reporter/0.1 (connor.razo@bsu.edu)");
         InputStream inputStream = connectionForArticle.getInputStream();
 
-        return new WikiApiJsonTranslator(inputStream.toString()).formatInformation();
+        Scanner streamScanner = new Scanner(inputStream);
+        StringBuilder jsonFromScanner = new StringBuilder();
+        while (streamScanner.hasNextLine())
+            jsonFromScanner.append(streamScanner.nextLine());
+
+        return new WikiApiJsonTranslator(jsonFromScanner.toString()).formatInformation();
     }
-    public static WebPageData testingFetchArticleInfoWithFilename(String json) throws Exception {
+    public static WebPageData testingFetchArticleInfoWithJson(String json) throws Exception {
         return new WikiApiJsonTranslator(json).formatInformation();
     }
 
